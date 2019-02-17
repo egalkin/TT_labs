b7cc15b46b1cbc93976973dfaa0d70c423966d9f
Egor Galkin <egor.galkin99@yandex.ru>
2019-02-18 00:31:25 +0300

    Update code

--- Typing/src/Main.hs
+++ Typing/src/Main.hs
@@ -7,19 +7,12 @@ import Lexer (alexScanTokens)
 import Parser (parseExpr)
 import Data.List (elemIndex)
 import qualified Data.Set as Set
-import qualified Data.Map.Lazy as Map
+import qualified Data.Map as Map
 
 setNewName :: String -> Set.Set String -> String
 setNewName newName usedVarsNames  
     | not (Set.member newName usedVarsNames) = newName
     | otherwise                              = setNewName (newName ++ "'") usedVarsNames
-
-
-getContext :: UnificatorPair -> Context
-getContext (UnificatorPair cont varName) =  cont
-
-getVarType :: UnificatorPair -> Statement 
-getVarType (UnificatorPair cont statement) = statement
 
 
 uniteContexts :: Context -> Context -> Equation -> Context
@@ -35,14 +28,12 @@ statementSubstitution a omega curVar@(VarStatement var)
     | a == curVar                                          = omega
     | otherwise                                            = curVar
 
-substitutionStep :: Equation -> Statement -> Statement -> Equation -> Equation
-substitutionStep equ a omega curEqu@(Equation stat1 stat2)
-    | equ /= curEqu = Equation (statementSubstitution a omega stat1) (statementSubstitution a omega stat2) 
-    | otherwise     = equ
+substitutionStep :: Statement -> Statement -> Equation -> Equation
+substitutionStep a omega curEqu@(Equation stat1 stat2) = Equation (statementSubstitution a omega stat1) (statementSubstitution a omega stat2) 
 
-substitute :: Equation -> Statement -> Statement -> [Equation] -> [Equation]
-substitute _ _ _ []           = []
-substitute equ a omega (x : xs) = substitutionStep equ a omega x : substitute equ a omega xs
+substitute :: Statement -> Statement -> [Equation] -> [Equation]
+substitute _ _ []           = []
+substitute a omega (x : xs) = substitutionStep a omega x : substitute a omega xs
 
 unificationStep :: Equation -> [Equation] -> [Equation]
 unificationStep (Equation (ImplicationStatement lp rp) (VarStatement var)) xs                 = 
@@ -55,8 +46,8 @@ unificationStep (Equation (ImplicationStatement lp1 rp1) (ImplicationStatement l
     doUnification xs ++ res
 unificationStep equ@(Equation a@(VarStatement x) omega@(VarStatement y)) xs 
     | x == y                                                                                 = doUnification xs
-    | otherwise                                                                              = doUnification (substitute equ a omega xs) ++ [equ]
-unificationStep equ@(Equation a@(VarStatement var) omega) xs                                 = doUnification (substitute equ a omega xs) ++ [equ]
+    | otherwise                                                                              = doUnification (substitute a omega xs) ++ [equ]
+unificationStep equ@(Equation a@(VarStatement var) omega) xs                                 = doUnification (substitute a omega xs) ++ [equ]
     
 
 
@@ -117,63 +108,169 @@ isIncompatible (x : xs) =
         returnOrContinue True _ _   = True
 
 systemSolvingLoop :: Context -> Int -> Maybe Context
-systemSolvingLoop (Context eqs) cnt = 
+systemSolvingLoop (Context eqs) cnt =
         let solvedContext = doUnification eqs
             incompatible  = isIncompatible solvedContext
-            in
-            fallOrContinue incompatible solvedContext
-            where 
-                fallOrContinue True _              = Nothing
-                fallOrContinue False solvedContext = 
-                    let solved = isSolved solvedContext (Set.fromList $ countAllMetVars $ solvedContext)
-                    in 
-                    returnOrContinue solvedContext solved
-                    where 
-                       returnOrContinue solvedContext True        = Just (Context  solvedContext)
-                       returnOrContinue solvedContext False       = systemSolvingLoop (Context solvedContext) (cnt+1)
+        in
+        fallOrContinue incompatible solvedContext
+        where 
+            fallOrContinue True solvedContext              = Nothing
+            fallOrContinue False solvedContext = 
+                let solved = isSolved solvedContext (Set.fromList $ countAllMetVars $ solvedContext)
+                in 
+                returnOrContinue solvedContext solved
+                where 
+                   returnOrContinue solvedContext True        = Just (Context  solvedContext)
+                   returnOrContinue solvedContext False       = systemSolvingLoop (Context solvedContext) (cnt+1)
 systemSolvingLoop _ _ = Just EmptyContext
 
-solveSystem :: Context -> Context
-solveSystem cont =
-    let systemSolution = systemSolvingLoop cont 0
-    in
-    getResult systemSolution
-    where
-        getResult (Just a) = a
-        getResult Nothing  = InvalidContext
+solveSystem :: Context -> Maybe Context
+solveSystem cont = systemSolvingLoop cont 0
 
-
-buildTypeUnificator :: Tree -> Set.Set String -> (UnificatorPair, Set.Set String)
-buildTypeUnificator (Var x) usedVarsNames = 
+buildTypeUnificator :: Tree -> Set.Set String -> Int -> (UnificatorPair, (Int, Set.Set String))
+buildTypeUnificator (Var x) usedVarsNames num = 
     let newUsedVars = (Set.insert x usedVarsNames)
-        newVarName = setNewName ("a" ++ x) newUsedVars
-        in
-        (UnificatorPair EmptyContext (VarStatement newVarName), Set.insert newVarName newUsedVars)
-buildTypeUnificator (ApplicationTree p q) usedVarsNames = 
-        let pResult         = buildTypeUnificator p usedVarsNames
-            qResult         = buildTypeUnificator q usedVarsNames
-            updatedUsedVars = Set.union (snd pResult) (snd qResult)
-            newVarName      = setNewName ("a" ++ "'") updatedUsedVars
+        newVarName  = setNewName ("a" ++ show num ++ x) newUsedVars
+    in
+    (UnificatorPair EmptyContext (VarStatement newVarName), (num, Set.insert newVarName newUsedVars))
+buildTypeUnificator (ApplicationTree p q) usedVarsNames num = 
+        let pResult         = buildTypeUnificator p usedVarsNames (num+1)
+            qResult         = buildTypeUnificator q usedVarsNames ((fst $ snd pResult) + 1)
+            updatedUsedVars = Set.union (snd $ snd pResult) (snd $ snd qResult)
+            newVarName      = setNewName ("a" ++ show (fst $ snd qResult) ++ "'") updatedUsedVars
             pType           = getVarType $ fst pResult
             qType           = getVarType $ fst qResult
             newUsedVars     = Set.insert newVarName updatedUsedVars 
-            in
-            (UnificatorPair 
-                (uniteContexts (getContext $ fst pResult) (getContext $ fst qResult) (Equation pType (ImplicationStatement qType (VarStatement newVarName))))
-                (VarStatement newVarName), newUsedVars)
-buildTypeUnificator (LambdaTree (Var x) p) usedVarsNames = 
-        let pResult    = buildTypeUnificator p usedVarsNames 
-            newVarName = "a" ++ x
-            in
-            (UnificatorPair (getContext $ fst pResult) (ImplicationStatement (VarStatement newVarName) (getVarType $ fst pResult)), usedVarsNames)
+        in
+        (UnificatorPair 
+            (uniteContexts (getContext $ fst pResult) (getContext $ fst qResult) (Equation pType (ImplicationStatement qType (VarStatement newVarName))))
+            (VarStatement newVarName), (fst $ snd qResult, newUsedVars))
+buildTypeUnificator (LambdaTree (Var x) p) usedVarsNames num = 
+        let pResult    = buildTypeUnificator p usedVarsNames num
+            newVarName = "a" ++ show num ++ x
+        in
+        (UnificatorPair (getContext $ fst pResult) (ImplicationStatement (VarStatement newVarName) (getVarType $ fst pResult)),(num, usedVarsNames))
 
+buildExprTypeTemplate :: Tree -> Set.Set String  -> Int-> (Statement, (Int, Set.Set String))
+buildExprTypeTemplate (Var x) usedVarsNames num = 
+    let newUsedVars = (Set.insert x usedVarsNames)
+        newVarName = setNewName ("a" ++ show num ++ x) newUsedVars
+    in
+    (VarStatement newVarName, (num, Set.insert newVarName newUsedVars))
+buildExprTypeTemplate (ApplicationTree p q) usedVarsNames num = 
+        let pResult         = buildExprTypeTemplate p usedVarsNames (num + 1)
+            qResult         = buildExprTypeTemplate q usedVarsNames ((fst $ snd pResult) + 1)
+            updatedUsedVars = Set.union (snd $ snd pResult) (snd $ snd qResult)
+            newVarName      = setNewName ("a" ++ show (fst $ snd qResult) ++ "'") updatedUsedVars
+            newUsedVars     = Set.insert newVarName updatedUsedVars 
+        in
+        (VarStatement newVarName, (fst $ snd qResult, newUsedVars))       
+buildExprTypeTemplate (LambdaTree (Var x) p) usedVarsNames num = 
+        let pResult    = buildExprTypeTemplate p usedVarsNames num
+            newVarName = "a" ++ show num ++  x
+        in
+        (ImplicationStatement (VarStatement newVarName) (fst pResult), (num, usedVarsNames))
 
-typing :: Tree -> Context 
-typing t =  solveSystem $ getContext $ fst $ buildTypeUnificator t Set.empty
+getContext :: UnificatorPair -> Context
+getContext (UnificatorPair cont varName) =  cont
+
+getVarType :: UnificatorPair -> Statement 
+getVarType (UnificatorPair cont statement) = statement
+
+getEquations :: Context -> [Equation]
+getEquations (Context eqs)  = eqs
+getEquations EmptyContext   = []
+
+buildVarTypesMap :: [Equation] -> Map.Map Statement Statement -> Map.Map Statement Statement
+buildVarTypesMap [] varsTypesMap                            = varsTypesMap
+buildVarTypesMap ((Equation stat1 stat2) : xs) varsTypesMap = 
+    let updatedMap = Map.insert stat1 stat2 varsTypesMap
+    in
+    buildVarTypesMap xs updatedMap
+
+substituteTypes :: Statement -> Map.Map Statement Statement -> Statement
+substituteTypes (ImplicationStatement stat1 stat2) vtMap = ImplicationStatement (substituteTypes stat1 vtMap) (substituteTypes stat2 vtMap)
+substituteTypes var@(VarStatement x) vtMap               =
+    let possibleTypeVar = Map.lookup var vtMap
+    in
+    updateTypeOrReturnPrev possibleTypeVar
+    where
+        updateTypeOrReturnPrev (Just a) = a
+        updateTypeOrReturnPrev Nothing  = var
+
+showTypeInferenceContext :: [TypePair] -> String
+showTypeInferenceContext []       = []
+showTypeInferenceContext (x : []) = show x ++ " "
+showTypeInferenceContext (x : xs) = show x ++ ", " ++ showTypeInferenceContext xs
+
+getTypeInferenceStep :: Int -> TypeInferenceContext -> TypePair -> Int -> String
+getTypeInferenceStep innerLevel tiContext exprTypePair ruleNum = 
+    getInferencePrefix innerLevel ++ (showTypeInferenceContext $ Set.toList tiContext) ++ "|- " ++ show exprTypePair ++ " [rule #" ++ show ruleNum ++ "]" 
+
+buildTypeInference :: Tree -> Map.Map Statement Statement -> TypeInferenceContext -> Int -> Int -> (Int, [String])
+buildTypeInference expr@(LambdaTree var@(Var x) p) vtMap tiContext innerLevel num = 
+    let typeTemplate = buildExprTypeTemplate expr Set.empty num
+        exprTypePair = TypePair expr (substituteTypes (fst $ typeTemplate) vtMap)
+        xTypePair    = TypePair var (substituteTypes (VarStatement ("a" ++ show num ++ x)) vtMap)
+        pTypeInf     = buildTypeInference p vtMap (Set.insert xTypePair tiContext) (innerLevel + 1) num
+    in
+    (num, getTypeInferenceStep innerLevel tiContext exprTypePair 3 : (snd pTypeInf))
+buildTypeInference expr@(ApplicationTree p q) vtMap tiContext innerLevel num      =
+    let typeTemplate = buildExprTypeTemplate expr Set.empty num
+        exprTypePair = TypePair expr (substituteTypes (fst $ typeTemplate) vtMap)
+        pTypeInf     = buildTypeInference p vtMap tiContext (innerLevel + 1) (num + 1)
+        qTypeInf     = buildTypeInference q vtMap tiContext (innerLevel + 1) ((fst pTypeInf) + 1)
+    in 
+    (fst qTypeInf, getTypeInferenceStep innerLevel tiContext exprTypePair 2 
+        : (snd pTypeInf ++ snd qTypeInf ))
+buildTypeInference expr@(Var x) vtMap tiContext innerLevel num                    =
+    let exprTypePair = TypePair expr (substituteTypes (VarStatement ("a" ++ show num ++ x)) vtMap) 
+    in 
+    (num, getTypeInferenceStep innerLevel (Set.insert exprTypePair tiContext) exprTypePair 1 : [])
+
+    
+getInferencePrefix :: Int -> String
+getInferencePrefix innerLevel = concat $ take innerLevel (repeat "*   ") 
+
+getFreeVars :: Tree -> Int -> (Int, Set.Set (String, Int))
+getFreeVars (LambdaTree (Var x) p) num = (num, Set.delete (x, num) (snd $ getFreeVars p num))
+getFreeVars (ApplicationTree p q)  num =
+    let pVars = getFreeVars p (num + 1)
+        qVars = getFreeVars q (fst pVars)
+    in 
+    (fst qVars, Set.union (snd pVars) (snd qVars))
+getFreeVars (Var x) num               = (num, Set.singleton (x, num))
+
+buildInferenceContext :: Tree -> Map.Map Statement Statement -> TypeInferenceContext
+buildInferenceContext expr vtMap = 
+    let freeVars = Set.toList $ snd $ getFreeVars expr 0
+    in
+    Set.fromList $ construct freeVars vtMap
+    where
+        construct [] _           = []
+        construct ((x,num) : xs) vtMap = (TypePair (Var x) (substituteTypes (VarStatement ("a" ++ show num ++  x)) vtMap)) : construct xs vtMap
+
+typing :: Tree -> String 
+typing expr = 
+    let unifPair          = fst $ buildTypeUnificator expr Set.empty 0
+        solvedSystem      = solveSystem $ getContext $ unifPair
+    in
+    buildTypeInferenceOrReturnNoTypeString solvedSystem (getVarType unifPair)
+    where
+        buildTypeInferenceOrReturnNoTypeString (Just solvedSystem) exprType = 
+            let varsTypesMap      = buildVarTypesMap (getEquations solvedSystem) Map.empty
+                resultedType      = substituteTypes exprType varsTypesMap
+                inferenceContext  = buildInferenceContext expr varsTypesMap 
+                typeInference     = buildTypeInference expr varsTypesMap inferenceContext 0 0
+            in
+            -- show inferenceContext 
+            unlines $ snd typeInference
+            -- show solvedSystem
+        buildTypeInferenceOrReturnNoTypeString Nothing _                     = "Expression has no type"
 
 main::IO()
 main = do 
   input <- getLine
   case parseExpr (alexScanTokens input) of
     Left err   -> putStrLn err
-    Right expr -> putStrLn $ show $ typing expr
+    Right expr -> putStr $ typing expr
--- Typing/src/Types.hs
+++ Typing/src/Types.hs
@@ -1,5 +1,6 @@
 module Types where
 
+import Grammar (Tree (..))
 import qualified Data.Set as Set
 
 data Context = Context [Equation]
@@ -9,7 +10,6 @@ data Context = Context [Equation]
 instance Show Context where
     show (Context eqs) = show eqs
     show EmptyContext  = "{}"
-    show InvalidContext = "InvalidContext"
 
 data Equation = Equation Statement Statement
 
@@ -23,12 +23,27 @@ instance Ord Equation where
     compare eq1 eq2 = compare (show eq1) (show eq2)
 
 data Statement = VarStatement String 
-               | ImplicationStatement Statement Statement deriving Eq
+               | ImplicationStatement Statement Statement deriving (Eq, Ord)
 
 
+type TypeInferenceContext = Set.Set TypePair
+
+
+data TypePair = TypePair Tree Statement
+
+instance Eq TypePair where
+    p1 == p2 = show p1 == show p2
+
+instance Ord TypePair where
+    compare p1 p2 = compare (show p1) (show p2)
+
+instance Show TypePair where
+    show (TypePair tree typeStat) = show tree ++ " : " ++ show typeStat
+ 
 instance Show Statement where
     show (VarStatement var) = var
-    show (ImplicationStatement var1 var2) = "->" ++ "(" ++ show var1  ++ " " ++ show var2 ++ ")"              
+    -- show (ImplicationStatement var1 var2) = "->" ++ "(" ++ show var1  ++ " " ++ show var2 ++ ")"              
+    show (ImplicationStatement var1 var2) = "(" ++ show var1  ++ "->" ++ show var2 ++ ")"              
 
 
 data UnificatorPair = UnificatorPair Context Statement 
